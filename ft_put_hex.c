015b73dfbd690aee16ad1c3bfa32ff2683091a0b
KruimelKoning <123164160+KruimelKoning@users.noreply.github.com>
2023-12-04 22:49:47 +0100

    Add files via upload

---
+++ ft_put_hex.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_put_hex.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lbartels <lbartels@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/01 16:57:13 by lbartels      #+#    #+#                 */
+/*   Updated: 2023/11/08 13:35:14 by lbartels      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	hex_length(unsigned int hex)
+{
+	int	len;
+
+	len = 0;
+	if (hex == 0)
+		len++;
+	while (hex != 0)
+	{
+		hex /= 16;
+		len++;
+	}
+	return (len);
+}
+
+int	ft_put_hex(unsigned int hex, char format)
+{
+	char	*hexa;
+	static int		i;
+	int		len;
+
+	len = hex_length(hex);
+	i = 0;
+	if (format == 'X')
+		hexa = "0123456789ABCDEF";
+	if (format == 'x')
+		hexa = "0123456789abcdef";
+	if (hex >= 16)
+	{
+		ft_put_hex(hex / 16, format);
+		ft_put_hex(hex % 16, format);
+	}
+	else if (hex < 16)
+		write (1, &hexa[hex], 1);
+	return (len);
+}
